a4023f75e94d14a8b854fa7112dcccb5ecbb606a
Kamal <kamal.kurbanov2018@gmail.com>
2025-02-18 18:53:03 +0300

    feat: added css optimization for devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,59 @@
-"use client"
+"use client";
 
 import HelloCardComponent from "insan/components/hello-card.component";
 import StatCardComponent from "insan/components/stat-card.component";
 import WorkCardComponent from "insan/components/work-card.component";
 
 const Home = () => {
-
-    return (
-        <div className='p-10'>
-            <HelloCardComponent/>
-            <div className='stat-card-list'>
-                <StatCardComponent numberStat={'1 023 997 221'} textStat={'Рублей было собрано на благотворительность'} />
-                <StatCardComponent numberStat={'203 737'} textStat={'Человек получили помощь'} />
-                <StatCardComponent numberStat={'152 252'} textStat={'Человек поддержали работу фонда'} />
-            </div>
-            <h1 className='title-text'>Наша работа</h1>
-            <div className='work-card-list'>
-                <WorkCardComponent url={''} image='/hello-card-bg.jpg' text={"Спасибо всем ,кто принял участие в данном богоугодном деле. Пусть Всевышний примет, и приумножит. Амин !"}/>
-                <WorkCardComponent url={''} image='/hello-card-bg.jpg' text={`СБОР ЗАВЕРШЕН!
+  return (
+    <div className="app-content">
+      <HelloCardComponent />
+      <div className="stat-card-list">
+        <StatCardComponent
+          numberStat={"1 023 997 221"}
+          textStat={"Рублей было собрано на благотворительность"}
+        />
+        <StatCardComponent
+          numberStat={"203 737"}
+          textStat={"Человек получили помощь"}
+        />
+        <StatCardComponent
+          numberStat={"152 252"}
+          textStat={"Человек поддержали работу фонда"}
+        />
+      </div>
+      <h1 className="title-text">Наша работа</h1>
+      <div className="work-card-list">
+        <WorkCardComponent
+          url={""}
+          image="/hello-card-bg.jpg"
+          text={
+            "Спасибо всем ,кто принял участие в данном богоугодном деле. Пусть Всевышний примет, и приумножит. Амин !"
+          }
+        />
+        <WorkCardComponent
+          url={""}
+          image="/hello-card-bg.jpg"
+          text={`СБОР ЗАВЕРШЕН!
                     Выражаем слова благодарности всем,кто не остался равнодушным к семье Магомедовых.\n
-                    Общими усилиями было собрано 181,400 тысяча рублей,которые были Общими усилиями было собрано 181,400 тысяча рублей,которые были Общими усилиями было собрано 181,400 тысяча рублей,которые были Общими усилиями было собрано 181,400 тысяча рублей,которые были`}/>
-                <WorkCardComponent url={''} image='/hello-card-bg.jpg' />
-            </div>
-            <h1 className='title-text'>Руководство</h1>
-            <div className='management'>
-                <div className='management-head'>
-                    <div className='management-head_img'/>
-                    <h3>ПРЕЗИДЕНТ</h3>
-                    <h2>Джаватханов Закарияв Гаджимагомедович</h2>
-                </div>
-                <div className='management-info'>
-                    <h2>ОГРН 1186196033176</h2>
-                    <h2>ИНН 6150097063</h2>
-                </div>
-            </div>
+                    Общими усилиями было собрано 181,400 тысяча рублей,которые были Общими усилиями было собрано 181,400 тысяча рублей,которые были Общими усилиями было собрано 181,400 тысяча рублей,которые были Общими усилиями было собрано 181,400 тысяча рублей,которые были`}
+        />
+        <WorkCardComponent url={""} image="/hello-card-bg.jpg" />
+      </div>
+      <h1 className="title-text">Руководство</h1>
+      <div className="management">
+        <div className="management-head">
+          <div className="management-head_img" />
+          <h3>ПРЕЗИДЕНТ</h3>
+          <h2>Джаватханов Закарияв Гаджимагомедович</h2>
         </div>
-    );
-}
+        <div className="management-info">
+          <h2>ОГРН 1186196033176</h2>
+          <h2>ИНН 6150097063</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/nav-bar.component.tsx
+++ src/components/nav-bar.component.tsx
@@ -1,21 +1,93 @@
-"use client"
+"use client";
 
-import {usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { X, Menu } from "lucide-react";
 
 const NavBarComponent = () => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const pathname = usePathname()
+  const openBurger = () => {
+    setIsOpen((prev) => !prev);
+    document.body.classList.toggle("lock");
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-    return <nav className='w-full flex flex-row items-center py-12 p-10'>
-        <Link href='/'><Image className='items-start' src='./logo.svg' alt='Инсан' width={200} height={60}/></Link>
-        <ul className='w-full flex flex-row justify-evenly'>
-            <li><Link className={pathname === '/programs' ? 'nav-link active' : 'nav-link'} href={'/programs'}>Программы фонда</Link></li>
-            <li><Link className={pathname === '/zakat' ? 'nav-link active' : 'nav-link'} href={'/zakat'}>Выплата закята</Link></li>
-            <li><Link className={pathname === '/help-me' ? 'nav-link active' : 'nav-link'} href={'/help-me'}>Мне нужна помощь</Link></li>
-        </ul>
+  return (
+    <nav className="nav w-full flex flex-row items-center py-12 p-10">
+      <Link href="/" className="nav-logo">
+        <Image
+          className="items-start"
+          src="./logo.svg"
+          alt="Инсан"
+          width={200}
+          height={60}
+        />
+      </Link>
+      <button className="nav-burger-button" onClick={openBurger}>
+        {isOpen ? <X size={30} /> : <Menu size={30} />}
+      </button>
+      <ul className={isOpen ? `nav-menu active` : "nav-menu"}>
+        <li>
+          <Link
+            className={
+              pathname === "/programs" ? "nav-link active" : "nav-link"
+            }
+            href={"/programs"}
+          >
+            Программы фонда
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={pathname === "/zakat" ? "nav-link active" : "nav-link"}
+            href={"/zakat"}
+          >
+            Выплата закята
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={pathname === "/help-me" ? "nav-link active" : "nav-link"}
+            href={"/help-me"}
+          >
+            Мне нужна помощь
+          </Link>
+        </li>
+      </ul>
+      <ul className={"nav-items w-full flex flex-row justify-evenly"}>
+        <li>
+          <Link
+            className={
+              pathname === "/programs" ? "nav-link active" : "nav-link"
+            }
+            href={"/programs"}
+          >
+            Программы фонда
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={pathname === "/zakat" ? "nav-link active" : "nav-link"}
+            href={"/zakat"}
+          >
+            Выплата закята
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={pathname === "/help-me" ? "nav-link active" : "nav-link"}
+            href={"/help-me"}
+          >
+            Мне нужна помощь
+          </Link>
+        </li>
+      </ul>
     </nav>
-}
+  );
+};
 
-export default NavBarComponent
+export default NavBarComponent;
