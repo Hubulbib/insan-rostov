e4e1ce40539052da4e645995341565af0075ed18
Kamal <kamal.kurbanov2018@gmail.com>
2025-03-06 17:33:48 +0300

    feat: added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://insan-rostov.ru", // Замените на ваш домен
+  generateRobotsTxt: true, // (опционально) создаст robots.txt
+  // ...другие настройки
+};
